55d82d00d1dec3a28d0db317949af5eb1848f696
Christopher williams <christopher@MacBook-Pro.local>
2020-10-11 22:41:14 -0400

    Finished closure MVP

--- challenges/closure.js
+++ challenges/closure.js
@@ -22,7 +22,13 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-function summation(counter) {
- 
+function summation(number) {
+  const counter = []
+  for (let i = 0; i <= number; i++) {
+  counter.push(i)    
+  }
+  const total = counter.reduce((accumulator, current) => accumulator + current);
+  return total;
 }
-let counter = 2
+
+console.log(summation(4))
